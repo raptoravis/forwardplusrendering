e687cebce0bb574a4e9190471ee1abcf972cd6f4
jonli <jonli@tencent.com>
2019-10-07 12:48:29 +0800

    launch.bat added

---
+++ launch.bat
@@ -0,0 +1,5 @@
+
+set FPR_TOP_DIR=%~dp0
+
+REM %FPR_TOP_DIR%\build\win64\Debug\forwardplusrendering.exe -c ./conf/testscene.3dgep
+%FPR_TOP_DIR%\build\win64\Debug\forwardplusrendering.exe -c ./conf/crytek-sponza.3dgep
